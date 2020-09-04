bcaf20c0ca6b823c20c5e58f199ad48beab16fc1
mbabali iryn <mbabaliiryn@gmail.com>
2020-09-04 12:50:39 +0300

    Updates

---
+++ precache-manifest.6015e19f8626fb387f8df366e99532a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "641c1f7348f283d1dd00d391e65422ec",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "83af0037b73b63ffa5db",
+    "url": "/react-calculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "968dd0f785dae409256c",
+    "url": "/react-calculator/static/js/2.a6f7bb06.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator/static/js/2.a6f7bb06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83af0037b73b63ffa5db",
+    "url": "/react-calculator/static/js/main.86835ccc.chunk.js"
+  },
+  {
+    "revision": "3c4ee116adfa9d8605e9",
+    "url": "/react-calculator/static/js/runtime-main.5a7a9823.js"
+  }
+]);
